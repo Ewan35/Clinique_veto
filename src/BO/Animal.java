ff9fc5302130ac305d814ba726e41066dac5d388
Ewan <penanchoat@gmail.com>
2018-06-25 16:56:55 +0200

    Attributs classe animal

--- src/BO/Animal.java
+++ src/BO/Animal.java
@@ -2,4 +2,14 @@ package BO;
 
 public class Animal {
 
+    private Integer code;
+    private String nom;
+    private String sexe;
+    private String couleur;
+    private String race;
+    private Integer espece;
+    private String codeClient;
+    private Integer tatouage;
+    private String antecedents;
+    private Short archive;
 }
