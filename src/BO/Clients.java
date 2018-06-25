e0b08db3b17c5f25304ce98cf6a19779028bf5ef
Ewan <penanchoat@gmail.com>
2018-06-25 16:47:12 +0200

    Création couches / classes

---
+++ src/BO/Clients.java
@@ -0,0 +1,16 @@
+package BO;
+
+public class Clients {
+    private Integer code;
+    private String nom;
+    private String prenom;
+    private String adresse1;
+    private String adresse2;
+    private Integer codePostal;
+    private String ville;
+    private Integer num;
+    private String assurance;
+    private String email;
+    private String remarque;
+    private Short archive;
+}
